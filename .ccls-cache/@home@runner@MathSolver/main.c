617c955044d2a658294d7293d0c1affaace049e6
Bernardo-07 <84275344+Bernardo-07@users.noreply.github.com>
2023-07-12 15:00:00 +0000

    Função Afim

--- .ccls-cache/@home@runner@MathSolver/main.c
+++ .ccls-cache/@home@runner@MathSolver/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello World\n");
+int afim(){
+  int a, b, x, y;
+  printf("Digite o valor de a: ");
+  scanf("%d",&a);
+  printf("Digite o valor de b: ");
+  scanf("%d",&b);
+  if(b>0){
+    printf("lei de Formação: f(x) = %dx +%d", a, b);
+  }else if(b==0){
+    printf("lei de Formação: f(x) = %dx", a);
+  }else{
+    printf("lei de Formação: f(x) = %dx %d", a, b);
+  }
+  printf("\n\nDigite o valor de x: ");
+  scanf("%d",&x);
+  y = (a*x)+b;
+  printf("f(%d) = %d", x, y);
+  return 0;
+}
+
+int main() {
+  afim();
   return 0;
 }
